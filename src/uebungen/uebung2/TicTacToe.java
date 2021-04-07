7942f45f4743fce43727128bbc4d50a7944c1539
inari <katrinmalfent@gmail.com>
2021-04-07 11:31:32 +0200

    Eclipse configuation

--- src/uebungen/uebung2/TicTacToe.java
+++ src/uebungen/uebung2/TicTacToe.java
@@ -1,5 +1,25 @@
 package uebungen.uebung2;
 
 public class TicTacToe {
+	enum State {EMPTY, RED, BLACK};
+    State[][] field;
 
+    public TicTacToe() {
+        field = new State[3][3];
+        for(int i=0; i<field.length; i++) {
+          for(int j=0; j<field[i].length; j++) {
+            field[i][j]=State.EMPTY;
+          }
+        }
+    }
+
+    public void makeMove(int i, int j, State player) {
+        if(field[i][j]==State.EMPTY && player!=State.EMPTY) {  
+            field[i][j]=player;
+        }
+    }
+    
+    public void print() {
+    	
+    }
 }
