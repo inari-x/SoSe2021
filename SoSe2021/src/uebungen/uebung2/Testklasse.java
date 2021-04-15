60d8abd8ad35d42d7417406248d3ef736df0d21b
inari <katrinmalfent@gmail.com>
2021-04-15 20:24:26 +0200

    ttt done

--- SoSe2021/src/uebungen/uebung2/Testklasse.java
+++ SoSe2021/src/uebungen/uebung2/Testklasse.java
@@ -2,30 +2,21 @@ package uebungen.uebung2;
 
 import java.lang.Enum;
 
-import uebungen.uebung2.TicTacToe.State;
 import java.util.Random;
+
+import uebungen.uebung2.TicTacToe.State;
 
 public class Testklasse {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		
-		TicTacToe playingField = new TicTacToe();
-		
-		// playingField.print();
-		Random r = new Random();
-		// while 
-		// auto increment a variable which counts from 0
-		// 0 / evens = red -> use modulo to calculate if odd or even
-		// odds = black
-		playingField.makeMove(r.nextInt(3), r.nextInt(3), State.RED);
-		playingField.makeMove(r.nextInt(3), r.nextInt(3), State.BLACK);
-		// fake game simulation
-		
-		//
-		playingField.print();
-			
-		
+		TicTacToe ttt = new TicTacToe();
+		ttt.print();
+		ttt.makeMove(1, 2, State.RED);
+		ttt.print();
+		ttt.makeMove(-4711, 12345, State.EMPTY);
+	
 	}
 
 }
--- SoSe2021/src/uebungen/uebung2/TicTacToe.java
+++ SoSe2021/src/uebungen/uebung2/TicTacToe.java
@@ -13,51 +13,72 @@ public class TicTacToe {
         }
     }
 
-    public void makeMove(int i, int j, State player) {
-        if(field[i][j]==State.EMPTY && player!=State.EMPTY) {  
-            field[i][j]=player; 
-        }
-    }
+    boolean makeMove(int i, int j, State player) {
+    	if(0<=i && j<this.field.length && 
+    			0<=i && j<this.field[i].length && 
+    			player!=State.EMPTY)
+    		{
+    			if(this.field[i][j]==State.EMPTY)
+    			{
+    				this.field[i][j]=player;
+    				return true;
+    			}
+    		}
+    		return false;
+    	}
+ 
 
     public void print() {
-    	
-    		
         for(int i=0; i<field.length; i++) {  // Loop durch Feld1
             for(int j=0; j<field[i].length; j++) { // Loop durch Feld2
-              // System.out.print( "[" + field[i][j] + "]" );  //Index von Feldern leer (solange)
-            	switch(field[i][j]) {
-	        		case EMPTY: System.out.print("[-]"); break;
-	        		case RED:   System.out.print("[x]"); break;
-	        		case BLACK: System.out.print("[o]"); break;
+            	if(this.field[i][j]==State.EMPTY) {
+            		System.out.print(" -");
             	}
-            	
-            	//System.out.println();
-              
-            	// System.out.print( "[" + field[i][j] + "]" );  //Index von Feldern leer (solange)
+            	else if(this.field[i][j]==State.RED) {
+            		System.out.print(" x");
+            	}
+            	else if(this.field[i][j]==State.BLACK) {
+            		System.out.print(" o");
+            	}
             }
             System.out.println();
-        
-    	}
+        }
+        System.out.println();
     }
     
-//    public void print() {
-//    	State s = State.EMPTY;
-//    	switch(s) {
-//    		case EMPTY: System.out.println("-"); break;
-//    		case RED:   System.out.println("x"); break;
-//    		case BLACK: System.out.println("o"); break;
-//    	}
-//    	System.out.println(s);
-//    }
-//    
-    // step 1 - generate tic tac toe fields
-    // begin loop
-    // step 2 - generate a "move"
-    // step 3 - case/switch if winner/undecided (how to calculate?)
-    // reloop
-    // end
+    boolean gewonnen(State player) {
+    	if(player==State.EMPTY) return false;
+    	for(int i = 0; i < this.field.length; i++) {
+    		if(this.field[i][0]==player && this.field[i][1] == player && this.field[i][2]==player) {
+    			return true;
+    		}
+    	}
+    	for(int j = 0; j < this.field.length; j++) {
+    		if(this.field[j][0]==player && this.field[j][1] == player && this.field[j][2]==player) {
+    			return true;
+    		}
+    	}
+    	if(this.field[2][0] == player && this.field[1][1] == player && this.field[0][2] == player) {
+    		return true;
+    	}
+    	return false;
+    }
     
-//    public boolean gewonnen() {
-//    	
-//    }
+    boolean gewonnen() {
+    	return (gewonnen(State.BLACK) || gewonnen(State.RED));
+    }
+    
+    boolean voll() {
+    	for(int i = 0; i < this.field.length; i++) {
+    		for(int j = 0; j < this.field.length; j++) {
+    			if(this.field[i][j]==State.EMPTY) return false;
+    		}
+    	}
+    	return true;
+    }
+    
+    boolean unentschieden() {
+    	return(this.voll() && !this.gewonnen());
+    }
 }
+ 
