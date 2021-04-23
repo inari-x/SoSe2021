bbd77f942ffb3e87260911384b48cb56d9af27f2
inari <katrinmalfent@gmail.com>
2021-04-23 16:16:38 +0200

    Aufgabe2 angefangen

---
+++ SoSe2021/src/aufgaben/aufgabe2/MyInteger.java
@@ -0,0 +1,9 @@
+package aufgaben.aufgabe2;
+
+public class MyInteger {
+	
+	static int MAX_VALUE;
+	static int MIN_VALUE;
+	private int value;
+
+}
---
+++ SoSe2021/src/aufgaben/aufgabe2/Testklasse.java
@@ -0,0 +1,10 @@
+package aufgaben.aufgabe2;
+
+public class Testklasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
